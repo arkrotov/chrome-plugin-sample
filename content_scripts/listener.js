7a301c4e9040037c3fd7b34becaae70359a34780
Artem Krotov <artem.krotov@eldorado.ru>
2017-07-13 19:50:57 +0300

    Распарсил добавляемый элемент в массив значений

--- content_scripts/listener.js
+++ content_scripts/listener.js
@@ -8,36 +8,20 @@ var observer = new MutationObserver(function(mutations) {
     var string = '';
     var matches = [];
     mutations.forEach(function(mutation) {
-        console.log("Hello!");
         string = mutation.nextSibling;
         let innerText = string.innerText;
         console.log(innerText);
-      /*  let split = innerText.split(" ");
-        console.log(innerText.split(" ")[0]);
-        console.log(innerText.split(" ")[7]);
-        console.log(innerText.split(" ")[1]);
-        console.log(innerText.split(" ")[2]);
-        console.log(innerText.split(" ")[3]);
-        console.log(innerText.split(" ")[4]);*/
-
-        /*
-         18:31:12 Ограбление было предотвращено, грабители пойманы Куш 0,001
-         Владелец  DEDmustDIE  Поместье  Хутор  ARMY of Wolf
-         */
-       /* console.log(typeof string);
-        matches = string.match('<li class="zebra" style=""><div class="task-event"><span class="date">(.*)<\/span> <a class="" href="(.*)">.*<\/a> (.*) <img src=".*"><br> Владелец <span class=".*"><\/span> (.*) <img src="\/i\/houses\/.*.png"> (.*) <img width="21" src=".*"> (.*) <img width="21" src=".*"> (.*)<\/div><\/li>');
-        console.log(matches);*/
-
-    /*    console.log(typeof string);
-        while(string.nextNode()) {
-            var node = treeWalker.currentNode;
-            console.log(node);
-        }*/
+        let match = innerText.match("(.*) Банда .*\nВладелец  (.*)  (.*)  (.*)");
+        for (let obj of match) {
+            console.log(obj)
+        }
     });
 });
 
+//(.*) Банда .*\nВладелец  (.*)  (.*)  (.*)
 // настраиваем наблюдатель
 var config = { attributes: true, childList: true, characterData: true }
 
 // передаем элемент и настройки в наблюдатель
 observer.observe(target, config);
+
